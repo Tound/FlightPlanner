9a0a0c9c1d27d4f5cdf322b4424500bd178727ab
Tound <tompound@ymail.com>
2021-02-19 21:36:21 +0000

    New Project

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,23 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("FlightPlanner V1.0");
+        BorderPane bp = new BorderPane();
+        Scene main = new Scene(bp,1280,720);
+        primaryStage.setScene(main);
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
